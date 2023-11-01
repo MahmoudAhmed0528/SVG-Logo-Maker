8cde973e83bb13e5ac26902b0779c7cc670558af
MahmoudAhmed <mahmood.adel@hotmail.com>
2023-10-31 23:41:02 -0400

    modify test file

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,19 +1,18 @@
-const Shapes = require("../lib/shapes");
+const { Circle, Square, Triangle } = require("../lib/shapes");
 
 describe("Shapes", () => {
   describe("Circle", () => {
     it("render method should generate correct SVG", () => {
-      const shapes = new Shapes();
+      const circle = new Circle();
       circle.setColor("red");
       const svg = circle.render();
       expect(svg).toEqual('<circle cx="100" cy="100" r="80" fill="red" />');
     });
   });
-});
-describe("Shapes", () => {
-  describe("square", () => {
+
+  describe("Square", () => {
     it("render method should generate correct SVG", () => {
-      const shapes = new Shapes();
+      const square = new Square();
       square.setColor("green");
       const svg = square.render();
       expect(svg).toEqual(
@@ -21,11 +20,10 @@ describe("Shapes", () => {
       );
     });
   });
-});
-describe("Shapes", () => {
-  describe("triangle", () => {
+
+  describe("Triangle", () => {
     it("render method should generate correct SVG", () => {
-      const shapes = new Shapes();
+      const triangle = new Triangle();
       triangle.setColor("blue");
       const svg = triangle.render();
       expect(svg).toEqual(
